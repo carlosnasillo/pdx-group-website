ae20f36627eb7bbd8a71c1ffd8996b969d335e96
Julien Déray <julien@deray.fr>
2016-03-08 12:39:08 +0000

    verifies if the email is valid

--- js/contact.js
+++ js/contact.js
@@ -16,26 +16,36 @@
       }
 
       event.preventDefault();
+
+      var emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+
       if (email.length > 0 && name.length > 0 && message.length > 0) {
          // prevent default submit behaviour
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
-          console.log(xmlhttp.status);
-          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            // Success message
-            $('#success').html("<div class='alert alert-success'>");
-            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-            $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
-            $('#success > .alert-success').append('</div>');
-            //clear all fields
-            $('#contactForm').trigger("reset");
-          }
-          else if (xmlhttp.status != 200 && xmlhttp.status != 0) {
-            // Fail message
+          if ( !emailRegex.test(email) ) {
             $('#success').html("<div class='alert alert-danger'>");
             $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-            $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please send it as an email to hello@latticemarkets.com!");
+            $('#success > .alert-danger').append("<strong>Please use a valid email.");
             $('#success > .alert-danger').append('</div>');
+          }
+          else {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+              // Success message
+              $('#success').html("<div class='alert alert-success'>");
+              $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+              $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+              $('#success > .alert-success').append('</div>');
+              //clear all fields
+              $('#contactForm').trigger("reset");
+            }
+            else if (xmlhttp.status != 200 && xmlhttp.status != 0) {
+              // Fail message
+              $('#success').html("<div class='alert alert-danger'>");
+              $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+              $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please send it as an email to hello@latticemarkets.com!");
+              $('#success > .alert-danger').append('</div>');
+            }
           }
         };
         var parameters = "email=" +(email)+ "&name=" +encodeURIComponent(name)+ "&message=" +encodeURIComponent(message);
